00d35a86e2aaa19b39291de408882d1c853c35b2
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-03-14 21:55:22 +0000

    Decorator has been improved.

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = args[0];
+            char dec = char.Parse(args[1]);
+            int i = int.Parse(args[2]);
+            s = Decor(s,dec,i);
+            Console.WriteLine(s);
+        }
+        private static string Decor ( string s , char dec, int i)
+        {
+            for (int j = 0; j < i; j++ )
+            {
+                s = $"{dec}{s}{dec}";
+            }
+        
+        return s;
+        }
+
+    }
+}
