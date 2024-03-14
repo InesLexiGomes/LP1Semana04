727307d9f6a302d4865e5d1eaf2b83314434c949
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-03-14 22:34:15 +0000

    Special program returns number of the special sequence.

---
+++ Special/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Special
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert the number of the special sequence: ");
+            int num = int.Parse(Console.ReadLine());
+            num = GetSpecial(num);
+            Console.WriteLine(num);
+        }
+        private static int GetSpecial(int n)
+        {
+            int res;
+            if (n == 0)
+            {
+                res = 0;
+            }
+            else if (n == 1)
+            {
+                res = 1;
+            }
+            else
+            {
+                res = GetSpecial(n-1) + 2* GetSpecial(n-2);
+            }
+            return res;
+        }
+    }
+}
