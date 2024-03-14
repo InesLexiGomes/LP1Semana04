48fa3b43266fada67883347c3f279c4726adcba4
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-03-14 22:01:06 +0000

    Commented the better decorator.

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -4,6 +4,10 @@ namespace BetterDecorator
 {
     class Program
     {
+        /// <summary>
+        /// Executa o principal, e chama os outros.
+        /// </summary>
+        /// <param name="args">string, char, int</param>
         static void Main(string[] args)
         {
             string s = args[0];
@@ -12,6 +16,13 @@ namespace BetterDecorator
             s = Decor(s,dec,i);
             Console.WriteLine(s);
         }
+        /// <summary>
+        /// Adiciona caracteres um certo numero de vezes.
+        /// </summary>
+        /// <param name="s"> string </param>
+        /// <param name="dec"> char </param>
+        /// <param name="i"> int </param>
+        /// <returns></returns>
         private static string Decor ( string s , char dec, int i)
         {
             for (int j = 0; j < i; j++ )
